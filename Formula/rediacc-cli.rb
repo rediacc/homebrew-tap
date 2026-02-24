13eceda994c629609c68b410095fa045be78581c
Muhammed Fatih Bayraktar <mfbayraktar@live.com>
2026-02-24 12:05:47 +0100

    feat: migrate download URLs from GitHub Releases to Cloudflare R2

--- Formula/rediacc-cli.rb
+++ Formula/rediacc-cli.rb
@@ -6,14 +6,14 @@ class RediaccCli < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/rediacc/console/releases/download/v#{version}/rdc-mac-arm64"
+      url "https://releases.rediacc.com/cli/v#{version}/rdc-mac-arm64"
       sha256 "ad7741ced7d68ef52c82a2a7ce9476346c41b8dc4d209fda93ba763eac2a7f76"
 
       def install
         bin.install "rdc-mac-arm64" => "rdc"
       end
     else
-      url "https://github.com/rediacc/console/releases/download/v#{version}/rdc-mac-x64"
+      url "https://releases.rediacc.com/cli/v#{version}/rdc-mac-x64"
       sha256 "60709af76b96a83dd961db9bd0d1a234cfc41fbf75fdb4def37cbd6351c10272"
 
       def install
@@ -24,14 +24,14 @@ class RediaccCli < Formula
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/rediacc/console/releases/download/v#{version}/rdc-linux-arm64"
+      url "https://releases.rediacc.com/cli/v#{version}/rdc-linux-arm64"
       sha256 "3628e8604fcbe0de524868280bdc75275110ee06efbc6bb40b42eb194975b6c9"
 
       def install
         bin.install "rdc-linux-arm64" => "rdc"
       end
     else
-      url "https://github.com/rediacc/console/releases/download/v#{version}/rdc-linux-x64"
+      url "https://releases.rediacc.com/cli/v#{version}/rdc-linux-x64"
       sha256 "85f7f5cf8a5953c733cc7c8545481c211d6380acab7616700bb2614edc4e9456"
 
       def install
